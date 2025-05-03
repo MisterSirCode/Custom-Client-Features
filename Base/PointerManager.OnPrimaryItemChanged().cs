fe854028c7cb9bba6ff5c4245cf1e08d318343e9
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-02 21:55:06 -0400

    Silly change

---
+++ Base/PointerManager.OnPrimaryItemChanged().cs
@@ -0,0 +1,36 @@
+private void OnPrimaryItemChanged(Item item) 
+{
+    if (item != null && (item.placeable || item.consumable)) {
+        this.primaryItemPlaceable = true;
+        if (item.tileable || item.spriteLayers == null || item.spriteLayers.Count == 0) {
+            tk2dSpriteCollectionData tk2dSpriteCollectionData = Singleton<AtlasManager>.main.Collection("inventory");
+            if (tk2dSpriteCollectionData.GetSpriteDefinition(item.inventorySpriteName) != null) {
+                this.placeSprite.SetSprite(tk2dSpriteCollectionData, item.inventorySpriteName);
+            }
+            else {
+                this.placeSprite.SetSprite(tk2dSpriteCollectionData, "inventory/ground/onyx");
+            }
+            float num = Singleton<AtlasManager>.main.BaseBlockScale() / Mathf.Max(this.placeSprite.GetCurrentSpriteDef().rect.width, this.placeSprite.GetCurrentSpriteDef().rect.height);
+            this.placeSprite.scale = Vector3.one * num;
+            FixedSize fixedSize;
+            fixedSize.width = 1;
+            fixedSize.height = 1;
+        }
+        else {
+            tk2dSpriteDefinition tk2dSpriteDefinition = item.guiSprite;
+            if (tk2dSpriteDefinition == null) {
+                Item.SpriteLayer spriteLayer = item.spriteLayers[0];
+                tk2dSpriteDefinition = spriteLayer.sprites[0].sprite;
+            }
+            if (tk2dSpriteDefinition != null) {
+                tk2dSpriteCollectionData collection = tk2dSpriteDefinition.collection;
+                this.placeSprite.SetSprite(collection, tk2dSpriteDefinition.name);
+                this.placeSprite.scale = Vector3.one;
+                FixedSize fixedSize = SpriteHelper.BlockSize(tk2dSpriteDefinition);
+            }
+        }
+    }
+    else {
+        this.primaryItemPlaceable = false;
+    }
+}
--- Base/PortalItem.Configure().cs
+++ Base/PortalItem.Configure().cs
@@ -1,42 +1,38 @@
-	public void Configure(Dictionary<string, object> _config)
-	{
-		this.config = _config;
-		string @string = this.config.GetString("activity", (!this.config.GetBool("pvp", false)) ? null : "PvP");
-		this.biomeIcon.sprite = GameGui.GetSprite("biome-" + this.config.GetString("biome"));		
-        if (this.config.GetString("biome") == "ocean") {
-			Sprite sprite = ExternalSpriteLoader.GetSprite("biome-ocean");
-			this.biomeIcon.sprite = sprite;
-		}
-		if (this.config.GetString("activity") == "market") {
-			Sprite sprite2 = ExternalSpriteLoader.GetSprite("market-world");
-			this.biomeIcon.sprite = sprite2;
-		}
-		this.primaryLabel.text = this.config.GetString("name") + ((@string == null) ? string.Empty : string.Concat(new string[] {
-			" <color=#",
-			GameGui.AltTitleColorHex,
-			">",
-			@string.Capitalize(),
-			"</color>"
-		}));
-		if (this.config.GetString("gen_date") != null)
-		{
-			DateTime dateTime = DateTime.Parse(this.config.GetString("gen_date"));
-			this.dateLabel.text = dateTime.ToString("MMM").ToUpper() + "\n" + dateTime.ToString("yy");
-		}
-		else
-		{
-			this.dateLabel.text = "?";
-		}
-		this.lockedIcon.enabled = this.config.GetBool("protected", false);
-		this.bonusIcon.enabled = !this.lockedIcon.enabled && this.config.GetBool("premium", false);
-		if (this.config.GetString("name") != ReplaceableSingleton<Zone>.main.name)
-		{
-			int @int = this.config.GetInt("players", 0);
-			this.secondaryLabel.text = ((@int <= 0) ? "No occupants" : (@int + " occupant" + ((@int <= 1) ? string.Empty : "s")));
-		}
-		else
-		{
-			this.secondaryLabel.text = "You are here.";
-		}
-		this.chartOne.fillAmount = this.config.GetFloat("explored", 0f);
+public void Configure(Dictionary<string, object> _config) 
+{
+	this.config = _config;
+	string @string = this.config.GetString("activity", (!this.config.GetBool("pvp", false)) ? null : "PvP");
+	this.biomeIcon.sprite = GameGui.GetSprite("biome-" + this.config.GetString("biome"));		
+	if (this.config.GetString("biome") == "ocean") {
+		Sprite sprite = ExternalSpriteLoader.GetSprite("biome-ocean");
+		this.biomeIcon.sprite = sprite;
 	}
+	if (this.config.GetString("activity") == "market") {
+		Sprite sprite2 = ExternalSpriteLoader.GetSprite("market-world");
+		this.biomeIcon.sprite = sprite2;
+	}
+	this.primaryLabel.text = this.config.GetString("name") + ((@string == null) ? string.Empty : string.Concat(new string[] {
+		" <color=#",
+		GameGui.AltTitleColorHex,
+		">",
+		@string.Capitalize(),
+		"</color>"
+	}));
+	if (this.config.GetString("gen_date") != null) {
+		DateTime dateTime = DateTime.Parse(this.config.GetString("gen_date"));
+		this.dateLabel.text = dateTime.ToString("MMM").ToUpper() + "\n" + dateTime.ToString("yy");
+	}
+	else {
+		this.dateLabel.text = "?";
+	}
+	this.lockedIcon.enabled = this.config.GetBool("protected", false);
+	this.bonusIcon.enabled = !this.lockedIcon.enabled && this.config.GetBool("premium", false);
+	if (this.config.GetString("name") != ReplaceableSingleton<Zone>.main.name) {
+		int @int = this.config.GetInt("players", 0);
+		this.secondaryLabel.text = ((@int <= 0) ? "No occupants" : (@int + " occupant" + ((@int <= 1) ? string.Empty : "s")));
+	}
+	else {
+		this.secondaryLabel.text = "You are here.";
+	}
+	this.chartOne.fillAmount = this.config.GetFloat("explored", 0f);
+}
