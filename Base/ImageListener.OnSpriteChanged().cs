cd8c696a9fee450ea74c57509ff20b85e08afb0f
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-03 21:39:40 -0400

    Add onyx crow display to name

---
+++ Base/ImageListener.OnSpriteChanged().cs
@@ -0,0 +1,19 @@
+private void OnSpriteChanged(object spriteObj) {
+    string text = (string)spriteObj;
+    ExternalConsole.Log(text, spriteObj.ToString());
+    if (text != null && text.Length > 0) {
+        if (text.Contains("crow-5")) {
+            this.image.sprite = ExternalSpriteLoader.GetSprite("crow-5");
+            base.gameObject.SetActive(true);
+            return;
+        } else {
+            if (this.replace != null && this.replace.Length == 2) {
+                text = text.Replace(this.replace[0], this.replace[1]);
+            }
+            this.image.sprite = GameGui.GetSprite(text);
+            base.gameObject.SetActive(true);
+            return;
+        }
+    }
+    base.gameObject.SetActive(false);
+}
