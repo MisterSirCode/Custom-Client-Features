9617f7e403b4803636f339dc5d9f5126d0d7682d
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-06 10:37:06 -0400

    Create ExternalAtlas.cs

---
+++ Base/ExternalAtlas.cs
@@ -0,0 +1,11 @@
+public class ExternalAtlas {
+    public ExternalAtlas(string name, ExternalAsset image, ExternalAsset text) {
+        this.name = name;
+        this.sprite = image;
+        this.atlas = text;
+    }
+    public string name { get; set; }
+    public ExternalAsset sprite { get; }
+    public ExternalAsset atlas { get; }
+    public override string ToString() => $"Atlas ${this.name} (Image {this.sprite.name}, Text {this.atlas.name})";
+}
