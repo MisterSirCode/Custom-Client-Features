787d2ef8873aa058e3d7a54bcb4a15fdc1947403
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-06 09:17:04 -0400

    Finish up beta panel features

--- Base/HomeNewsPanel.RequestNews().cs
+++ Base/HomeNewsPanel.RequestNews().cs
@@ -2,6 +2,15 @@ private void RequestNews() {
     Action<WWWRequest> action = delegate(WWWRequest req) {
         if (req.response != null) {
             this.LoadNews(req.response.GetList("posts"));
+            if (req.response.GetString("beta_title") != null) {
+                this.betaPanelBanner.GetComponent<Text>().text = req.response.GetString("beta_title");
+                this.betaPanelContent.GetComponent<Text>().text = req.response.GetString("beta_content");
+                if (req.response.GetString("beta_button") != null) {
+                    this.betaPanelButtonText.GetComponent<Text>().text = req.response.GetString("beta_button");
+                    this.betaPanelButton.active = true;
+                    this.betaPanelButton.GetComponent<Hyperlink>().url = req.response.GetString("beta_hyperlink");
+                }
+            }
         }
     };
     Singleton<GameManager>.main.CreateGatewayRequest("clients", new Hashtable(), action, HTTPMethods.Get);
