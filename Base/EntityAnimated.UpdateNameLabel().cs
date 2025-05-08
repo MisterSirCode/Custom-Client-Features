cd26e78c3fda8b0d9cbf070a22be2390f62e4868
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-08 09:24:37 -0400

    Create EntityAnimated.UpdateNameLabel().cs

---
+++ Base/EntityAnimated.UpdateNameLabel().cs
@@ -0,0 +1,23 @@
+public void UpdateNameLabel() {
+    if (this.isPlayer || !this.config.named) {
+        return;
+    }
+    if (this.nameLabel == null) {
+        this.nameLabel = Singleton<EntityNameLabelPool>.main.Spawn();
+    }
+    this.nameLabel.name = ((this.nameIcon == null) ? base.name : ("   " + base.name));
+    if (this.nameIcon != null) {
+        if (this.nameIcon.Contains("crow-5")) {
+            this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+            this.nameLabel.iconName = "onyx-crow";
+            this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+        } else {
+            this.nameLabel.iconName = ((this.nameIcon == null) ? null : this.nameIcon.Replace("orders/", "order-"));
+            if (this.nameLabel.iconSprite) {
+                this.nameLabel.iconSprite.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+    }
+    this.nameLabel.nameLabel.color = ((!this.nameHighlighted) ? Color.white : GraphicsHelper.HexColor(GameGui.TitleColorHex));
+    base.StartCoroutine(this.UpdateDistance());
+}
