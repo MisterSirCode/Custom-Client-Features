2f659e84e281f804c3b8e9e7795d4858cc59bec5
Taylor Schneider <taylorschneider07@yahoo.com>
2025-05-18 12:17:30 -0400

    Update GameManager.IsSteamdeck().cs

--- Base/GameManager.IsSteamdeck().cs
+++ Base/GameManager.IsSteamdeck().cs
@@ -1,3 +1,3 @@
 public static bool IsSteamdeck() {
-		return ExternalControllerManager.instance.ControllerEnabled() && (PlayerPrefs.HasKey("controllerEnabled") ? PlayerPrefs.GetInt("controllerEnabled") == 1 : true);
+	return ExternalControllerManager.instance.ControllerEnabled() && (PlayerPrefs.HasKey("controllerEnabled") ? PlayerPrefs.GetInt("controllerEnabled") == 1 : false);
 }
