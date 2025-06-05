fd1e84d3fdb9d92de711568d806bed0222749178
Taylor Schneider <taylorschneider07@yahoo.com>
2025-06-05 18:29:49 -0400

    Fix the "crow bug" - Replace icon system.

--- Base/EntityAnimated.UpdateNameLabel().cs
+++ Base/EntityAnimated.UpdateNameLabel().cs
@@ -8,21 +8,12 @@ public void UpdateNameLabel() {
     }
     this.nameLabel.name = ((this.nameIcon == null) ? base.name : ("   " + base.name));
     if (this.nameIcon != null) {
-        if (this.nameIcon.Contains("crow-5")) {
-            this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
-            this.nameLabel.iconName = "onyx-crow";
-            this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
-        } else {
-			this.nameLabel.iconSprite.Collection = this.oldCollection;
-            this.nameLabel.iconName = ((this.nameIcon == null) ? null : this.nameIcon.Replace("orders/", "order-"));
-            if (this.nameLabel.iconSprite) {
-                this.nameLabel.iconSprite.transform.localScale = new Vector3(1f, 1f, 1f);
-            }
-        }
+        this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+        this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+        this.nameLabel.iconName = ((this.nameIcon == null) ? null : this.nameIcon.Replace("orders/", ""));
     } else {
-        this.nameLabel.iconSprite.Collection = null;
-        this.nameLabel.iconName = "";
-	}
+        this.nameLabel.iconName = null;
+    }
     this.nameLabel.nameLabel.color = ((!this.nameHighlighted) ? Color.white : GraphicsHelper.HexColor(GameGui.TitleColorHex));
     base.StartCoroutine(this.UpdateDistance());
 }
