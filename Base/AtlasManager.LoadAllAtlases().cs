6e6a90ad57236ba5497a5d8eddde6913108c2590
Taylor Schneider <taylorschneider07@yahoo.com>
2025-06-07 05:54:08 -0400

    MInor Adjustments

--- Base/AtlasManager.LoadAllAtlases().cs
+++ Base/AtlasManager.LoadAllAtlases().cs
@@ -1,7 +1,9 @@
 public void LoadAllAtlases() {
     if (ExternalAtlasLoader.instance != null && ExternalAtlasLoader.instance.atlases.Count > 0) {
-        foreach(ExternalAtlas atlas in ExternalAtlasLoader.instance.atlases) {
-            this.LoadExternalAtlas(atlas, true, true);
+        foreach (ExternalAtlas externalAtlas in ExternalAtlasLoader.instance.atlases) {
+            if (!externalAtlas.name.Contains("_entity")) {
+                this.LoadExternalAtlas(externalAtlas, "Graphics/WorldSpriteMaterial");
+            }
         }
     }
 }
