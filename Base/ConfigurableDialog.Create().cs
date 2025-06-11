9f66adecb6c94a6e67ca94e0e86cf093ebbc6427
Latte <mertborainevi@gmail.com>
2025-06-11 22:54:12 +0200

    memorize android dialog size

---
+++ Base/ConfigurableDialog.Create().cs
@@ -0,0 +1,38 @@
+public static ConfigurableDialog Create(Dictionary<string, object> config, ConfigurableDialog.ActionHandler successHandler = null, ConfigurableDialog.ActionHandler cancelHandler = null) {
+    string @string = config.GetString("type");
+    string text;
+    if (@string != null) {
+        if (@string == "android") {
+            text = "Prefabs/Dialogs/Configurable Android";
+            goto IL_008E;
+        }
+        if (@string == "loot") {
+            text = "Prefabs/Dialogs/Configurable Loot";
+            goto IL_008E;
+        }
+        if (@string == "loot_red") {
+            text = "Prefabs/Dialogs/Configurable Loot Red";
+            goto IL_008E;
+        }
+        if (@string == "loot_mech") {
+            text = "Prefabs/Dialogs/Configurable Loot Mech";
+            goto IL_008E;
+        }
+    }
+    text = "Prefabs/Dialogs/Configurable";
+    IL_008E:
+    GameObject dialog = (GameObject)global::UnityEngine.Object.Instantiate(Resources.Load(text));
+
+    // For some types of dialog try to resize according to preference
+    string dialogSizeId =@string ?? "standard";
+    GuiWindow guiWindow = dialog.GetComponent<GuiWindow>();
+    guiWindow.windowGroup = dialogSizeId;
+    if(dialogSizeId == "android") {
+        guiWindow.autoSavePosition = true;
+        guiWindow.RetrievePosition();
+    }
+    
+    ConfigurableDialog component = dialog.GetComponent<ConfigurableDialog>();
+    component.Show(config, successHandler, cancelHandler);
+    return component;
+}
