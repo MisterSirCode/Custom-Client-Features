cebd7b48f3f5a45c64b201b9351674db23931be9
Taylor Schneider <taylorschneider07@yahoo.com>
2025-06-12 17:20:38 -0400

    Remove stupid extra zone configurer call to load sprites

--- Base/Zone.Configure().cs
+++ Base/Zone.Configure().cs
@@ -8,12 +8,6 @@ public void Configure(Dictionary<string, object> config) {
     this.seed = int.Parse(this.documentId.Substring(2, 6), NumberStyles.HexNumber);
     this.biome = config.GetString("biome");
     Config.main.LoadBiome(this.biome);
-    if (this.biome != "ocean") {
-        Singleton<AtlasManager>.main.LoadTexture("biome-" + ((!(this.biome == "plain")) ? this.biome : "temperate"));
-        if (this.biome != "deep" && this.biome != "space") {
-            Singleton<AtlasManager>.main.LoadTexture("biome-" + ((!(this.biome == "plain")) ? this.biome : "temperate") + "-background");
-        }
-    }
     Physics.gravity = new Vector3(0f, -9.81f * ((!(this.biome == "space")) ? 1f : 0.7f), 0f);
     this.isMember = config.GetBool("member", false);
     this.isPrivate = config.GetBool("private", false);
