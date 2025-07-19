ef0332e0545e87d2ac349d1bba429576f9c38eb7
Latte <mertborainevi@gmail.com>
2025-07-20 01:18:19 +0200

    add separate firing interval specification

---
+++ Base/EntityHuman.ReadyGun().cs
@@ -0,0 +1,33 @@
+public bool ReadyGun(Item item, Vector2 pt) {
+    this.AnimateTool(pt);
+    this.UpdateArms(1f);
+    EntityConfig entityConfig = Config.main.entitiesByName.Get(item.spawn);
+    if (entityConfig != null) {
+        float firingInterval;
+
+        if (item.firingInterval < 0) {
+            float num = item.rate;
+            if (num == 0f) {
+                return false;
+            }
+            
+            if (!this.isPlayer) {
+                float num2 = (float)Config.PlayerSettings.quality * 0.4f + (float)ReplaceableSingleton<Ecosystem>.main.entityBulletCount * 0.003f;
+                num = Mathf.Lerp(num, num * 0.333f, num2);
+            }
+
+            firingInterval = 1f / num;
+        } else {
+            firingInterval = item.firingInterval;
+        }
+
+        if (PlayerDevice.isTouchScreen) {
+            firingInterval /= 0.8f;
+        }
+
+        if (Time.time < this.lastShootAt + firingInterval) {
+            return false;
+        }
+    }
+    return true;
+}
--- Base/Item.Item().cs
+++ Base/Item.Item().cs
@@ -2,5 +2,6 @@
 
 		this.scrubbable = base.ConfigBool("scrubbable", false);
 		this.jiggle = base.ConfigFloat("jiggle", 0.0f);
+		this.firingInterval = this.data.ContainsKey("firing_interval") ? (float)this.data.Get("firing_interval") : -1f;
 
 ...
