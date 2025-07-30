1860dc5e266652b2f2887e14e68fe58514f46d89
Latte <mertborainevi@gmail.com>
2025-07-31 01:19:55 +0200

    Add emitter_position support for guns

--- Base/EntityAvatar.EmitSteampackParticles().cs
+++ Base/EntityAvatar.EmitSteampackParticles().cs
@@ -11,12 +11,7 @@ private void EmitSteampackParticles() {
     }
     
     if (this.emitter != null && Time.time - this.lastSteamParticleAt > 1f / this.steamRate) {
-        Vector2 emitterPosition = this.steampackItem.emitterPosition;
-        float[] slotVertices = base.GetSlotVertices("suit");
-        Vector2 p0 = new Vector2(slotVertices[6], slotVertices[7]);
-        Vector2 px = new Vector2(slotVertices[0], slotVertices[1]);
-        Vector2 py = new Vector2(slotVertices[4], slotVertices[5]);
-        Vector2 pointOnSlot = p0 + emitterPosition.x * (px - p0) + emitterPosition.y * (py - p0);
+        Vector2 pointOnSlot = base.GetPointOnSlotAtRelativePosition("suit", this.steampackItem.emitterPosition);
         this.emitter.Spawn(pointOnSlot.x, pointOnSlot.y, 1);
         this.lastSteamParticleAt = Time.time;
     }
