dbd35656cb75fc2c562d114afdb1d93a8557c995
Taylor Schneider <taylorschneider07@yahoo.com>
2025-08-29 15:19:02 -0400

    Update Item.Item().cs

--- Base/Item.Item().cs
+++ Base/Item.Item().cs
@@ -1,18 +1,18 @@
 ...
 
-    this.scrubbable = base.ConfigBool("scrubbable", false);
-    this.jiggle = base.ConfigFloat("jiggle", 0.0f);
-    this.firingInterval = this.data.ContainsKey("firing_interval") ? (float)this.data.Get("firing_interval") : -1f;
+        this.scrubbable = base.ConfigBool("scrubbable", false);
+        this.jiggle = base.ConfigFloat("jiggle", 0.0f);
+        this.firingInterval = this.data.ContainsKey("firing_interval") ? (float)this.data.Get("firing_interval") : -1f;
 
-    List<object> emitterPosition = (List<object>)this.data.Get("emitter_position");
-    if (emitterPosition != null) {
-        this.emitterPosition = new Vector3((float)emitterPosition[0], (float)emitterPosition[1]);
-    } else {
-        if ("gun" == base.ConfigString("action")?.ToLower()) {
-            this.emitterPosition = new Vector3(0.5f, 0.0f);
+        List<object> emitterPosition = (List<object>)this.data.Get("emitter_position");
+        if (emitterPosition != null) {
+            this.emitterPosition = new Vector3((float)emitterPosition[0], (float)emitterPosition[1]);
         } else {
-            this.emitterPosition = new Vector3(0.5f, -0.4f);
+            if ("gun" == base.ConfigString("action")?.ToLower()) {
+                this.emitterPosition = new Vector3(0.5f, 0.0f);
+            } else {
+                this.emitterPosition = new Vector3(0.5f, -0.4f);
+            }
         }
-    }
 
 ...
