e08f2eb73c02e9945de2500689bb6fb9eddefa1e
Taylor Schneider <taylorschneider07@yahoo.com>
2025-08-29 15:33:00 -0400

    Fix formats, merge PR#7

--- Base/Player.TryToUseTool().cs
+++ Base/Player.TryToUseTool().cs
@@ -1,14 +1,14 @@
-	public void TryToUseTool(Vector2 blockPosition, Vector2 worldPosition) {
-		if (this.IsPrimaryItemGun()) {
-			this.TryToShoot(worldPosition);
-			return;
-		}
-        if (this.IsPrimaryItemScrubbingTool()) {
-            if (!this.TryToScrubBlock(blockPosition)) {
-			    this.SwingTool(worldPosition);
-            }
-        }
-		if (!this.TryToMineBlock(blockPosition)) {
+public void TryToUseTool(Vector2 blockPosition, Vector2 worldPosition) {
+	if (this.IsPrimaryItemGun()) {
+		this.TryToShoot(worldPosition);
+		return;
+	}
+	if (this.IsPrimaryItemScrubbingTool()) {
+		if (!this.TryToScrubBlock(blockPosition)) {
 			this.SwingTool(worldPosition);
 		}
 	}
+	if (!this.TryToMineBlock(blockPosition)) {
+		this.SwingTool(worldPosition);
+	}
+}
