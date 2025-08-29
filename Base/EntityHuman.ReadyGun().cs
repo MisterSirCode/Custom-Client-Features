fdc42d1d1664d6d95e098152403de5540779eac8
Taylor Schneider <taylorschneider07@yahoo.com>
2025-08-29 15:23:57 -0400

    fix formats and merge PR#6

--- Base/EntityHuman.ReadyGun().cs
+++ Base/EntityHuman.ReadyGun().cs
@@ -4,27 +4,22 @@ public bool ReadyGun(Item item, Vector2 pt) {
     EntityConfig entityConfig = Config.main.entitiesByName.Get(item.spawn);
     if (entityConfig != null) {
         float firingInterval;
-
         if (item.firingInterval < 0) {
             float num = item.rate;
             if (num == 0f) {
                 return false;
             }
-            
             if (!this.isPlayer) {
                 float num2 = (float)Config.PlayerSettings.quality * 0.4f + (float)ReplaceableSingleton<Ecosystem>.main.entityBulletCount * 0.003f;
                 num = Mathf.Lerp(num, num * 0.333f, num2);
             }
-
             firingInterval = 1f / num;
         } else {
             firingInterval = item.firingInterval;
         }
-
         if (PlayerDevice.isTouchScreen) {
             firingInterval /= 0.8f;
         }
-
         if (Time.time < this.lastShootAt + firingInterval) {
             return false;
         }
