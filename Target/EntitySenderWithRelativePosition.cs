a0371f5f7faff78b207293a2cf7f3a92badb9b79
Taylor Schneider <taylorschneider07@yahoo.com>
2025-08-29 15:16:26 -0400

    Last changes of PR#5 and merge

--- Target/EntitySenderWithRelativePosition.cs
+++ Target/EntitySenderWithRelativePosition.cs
@@ -1,38 +1,44 @@
-public class EntitySlotRelativePositionSender : EntityTargeter, Sender {
-    public EntitySlotRelativePositionSender(int entityId)
-        : base(entityId) {
-        this.slotRelativePosition = new Vector2(0.5f, 0);
-    }
+using UnityEngine;
+using System.Collections.Generic;
+using System;
 
-    public EntitySlotRelativePositionSender(Entity entity, Vector2 slotRelativePosition, string slot = null)
-        : base(entity, slot) {
-        this.slotRelativePosition = slotRelativePosition;
-    }
+namespace Target {
+    public class EntitySlotRelativePositionSender : EntityTargeter, Sender {
+        public EntitySlotRelativePositionSender(int entityId)
+            : base(entityId) {
+            this.slotRelativePosition = new Vector2(0.5f, 0);
+        }
 
-    public Vector2 Origin(Dictionary<string, object> details) {
-        if (!this.entity.IsVisible()) {
-            if (this.entity.isBlock) {
+        public EntitySlotRelativePositionSender(Entity entity, Vector2 slotRelativePosition, string slot = null)
+            : base(entity, slot) {
+            this.slotRelativePosition = slotRelativePosition;
+        }
+
+        public Vector2 Origin(Dictionary<string, object> details) {
+            if (!this.entity.IsVisible()) {
+                if (this.entity.isBlock) {
+                    return this.entity.Center();
+                }
+                return this.entity.cTransform.position;
+            } else {
+                if (this.slot == null) {
+                    object[] array = (object[])details.Get("sl");
+                    if (array != null) {
+                        int num = Convert.ToInt32(array[0]);
+                        this.slot = ((EntityAnimated)this.entity).GetConfigSlot(num);
+                    }
+                }
+                if (this.entity is EntityAnimated && this.slot != null) {
+                    return ((EntityAnimated)this.entity).GetPointOnSlotAtRelativePosition(this.slot, this.slotRelativePosition);
+                }
                 return this.entity.Center();
             }
-            return this.entity.cTransform.position;
-        } else {
-            if (this.slot == null) {
-                object[] array = (object[])details.Get("sl");
-                if (array != null) {
-                    int num = Convert.ToInt32(array[0]);
-                    this.slot = ((EntityAnimated)this.entity).GetConfigSlot(num);
-                }
-            }
-            if (this.entity is EntityAnimated && this.slot != null) {
-                return ((EntityAnimated)this.entity).GetPointOnSlotAtRelativePosition(this.slot, this.slotRelativePosition);
-            }
-            return this.entity.Center();
         }
-    }
 
-    public override bool Valid() {
-        return this.entity != null;
-    }
+        public override bool Valid() {
+            return this.entity != null;
+        }
 
-    private Vector2 slotRelativePosition;
+        private Vector2 slotRelativePosition;
+    }
 }
