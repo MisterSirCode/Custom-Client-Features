1650acac41a285bdcb99400c5a146d8ab8af7bbe
Taylor Schneider <taylorschneider07@yahoo.com>
2025-09-07 13:08:00 -0400

    Implement adaptive order icon handler

--- Base/ImageListener.OnSpriteChanged().cs
+++ Base/ImageListener.OnSpriteChanged().cs
@@ -1,13 +1,10 @@
 private void OnSpriteChanged(object spriteObj) {
     string text = (string)spriteObj;
     if (text != null && text.Length > 0) {
-		if (text.Contains("crow-1")) GameManager.order = "crow-1";
-		if (text.Contains("crow-2")) GameManager.order = "crow-2";
-		if (text.Contains("crow-3")) GameManager.order = "crow-3";
-		if (text.Contains("crow-4")) GameManager.order = "crow-4";
-		if (text.Contains("crow-5")) GameManager.order = "crow-5";
-        if (text.Contains("crow-5") && ExternalSpriteLoader.HasSprite("crow-5")) {
-            this.image.sprite = ExternalSpriteLoader.GetSprite("crow-5");
+        string clipped = text.Replace("emoji/orders/", "");
+        GameManager.order = clipped;
+        if (ExternalSpriteLoader.HasSprite(clipped)) {
+            this.image.sprite = ExternalSpriteLoader.GetSprite(clipped);
             base.gameObject.SetActive(true);
             return;
         } else {
