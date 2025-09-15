63f371818d8171df4b0286877cf002050369e756
Taylor Schneider <taylorschneider07@yahoo.com>
2025-09-14 20:52:03 -0400

    Derangement

--- Base/EntityAnimated.UpdateNameLabel().cs
+++ Base/EntityAnimated.UpdateNameLabel().cs
@@ -1,22 +1,48 @@
 public void UpdateNameLabel() {
-    if (this.isPlayer || !this.config.named) {
-        return;
-    }
-    if (this.nameLabel == null) {
-        this.nameLabel = Singleton<EntityNameLabelPool>.main.Spawn();
-			this.oldCollection = this.nameLabel.iconSprite.Collection;
-    }
-    this.nameLabel.name = ((this.nameIcon == null) ? base.name : ("   " + base.name));
-    if (this.nameIcon != null) {
-        this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
-        this.nameLabel.iconName = ((this.nameIcon == null) ? null : this.nameIcon.Replace("orders/", ""));
-        if (this.nameLabel.iconSprite) {
-            this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+    if (this.isPlayer) return;
+    if (this.config.named) {
+        if (this.nameLabel != null) {
+            if (this.nameIcon != null) {
+                if (this.nameIcon.Length > 0) {
+                    this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+                    this.nameLabel.name = "   " + base.name;
+                    this.nameLabel.iconName = this.nameIcon.Replace("orders/", "");
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+                } else {
+                    this.nameLabel.name = base.name;
+                    this.nameLabel.iconName = null;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+                }
+            } else {
+                this.nameLabel.name = base.name;
+                this.nameLabel.iconName = null;
+                this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+            }
+        } else {
+            this.nameLabel = Singleton<EntityNameLabelPool>.main.Spawn();
+            if (this.nameIcon != null) {
+                if (this.nameIcon.Length > 0) {
+                    this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+                    this.nameLabel.name = "   " + base.name;
+                    this.nameLabel.iconName = this.nameIcon.Replace("orders/", "");
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+                } else {
+                    this.nameLabel.name = base.name;
+                    this.nameLabel.iconName = null;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+                }
+            } else {
+                this.nameLabel.name = base.name;
+                this.nameLabel.iconName = null;
+                this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+            }
         }
     } else {
-        this.nameLabel.iconSprite.transform.localScale = new Vector3(1f, 1f, 1f);
-        this.nameLabel.iconName = null;
+        if (this.nameLabel != null || this.nameIcon == null || this.nameIcon.Length == 0) {
+            this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+            this.nameLabel.iconName = null;
+            this.nameLabel.name = null;
+        }
     }
-    this.nameLabel.nameLabel.color = ((!this.nameHighlighted) ? Color.white : GraphicsHelper.HexColor(GameGui.TitleColorHex));
     base.StartCoroutine(this.UpdateDistance());
 }
