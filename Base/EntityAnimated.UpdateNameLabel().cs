671d1364fa1718fe64bdbb7cc1a5b4b81be19e57
Taylor Schneider <taylorschneider07@yahoo.com>
2025-09-14 21:03:00 -0400

    May god forsake this demon

--- Base/EntityAnimated.UpdateNameLabel().cs
+++ Base/EntityAnimated.UpdateNameLabel().cs
@@ -1,9 +1,49 @@
 public void UpdateNameLabel() {
     if (this.isPlayer) return;
     if (this.config.named) {
-        if (this.nameIcon != null) {
-            if (this.nameIcon.Length > 0) {
+        if (this.nameLabel != null) {
+            if (this.nameIcon != null) {
+                if (this.nameIcon.Length > 0) {
+                    this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+                    this.nameLabel.name = "   " + base.name;
+                    this.nameLabel.iconName = this.nameIcon.Replace("orders/", "").Replace("crow-4", "crow-7");;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+                } else {
+                    this.nameLabel.name = base.name;
+                    this.nameLabel.iconName = null;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+                }
+            } else {
+                this.nameLabel.name = base.name;
+                this.nameLabel.iconName = null;
+                this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
             }
+        } else {
+            this.nameLabel = Singleton<EntityNameLabelPool>.main.Spawn();
+            if (this.nameIcon != null) {
+                if (this.nameIcon.Length > 0) {
+                    this.nameLabel.iconSprite.Collection = Singleton<AtlasManager>.main.Collection("customEmoji");
+                    this.nameLabel.name = "   " + base.name;
+                    this.nameLabel.iconName = this.nameIcon.Replace("orders/", "").Replace("crow-4", "crow-7");;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(5f, 5f, 1f);
+                } else {
+                    this.nameLabel.name = base.name;
+                    this.nameLabel.iconName = null;
+                    this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+                }
+            } else {
+                this.nameLabel.name = base.name;
+                this.nameLabel.iconName = null;
+                this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+            }
+        }
+    } else {
+        this.nameLabel.iconSprite.transform.localScale = new Vector3(1f, 1f, 1f);
+        this.nameLabel.iconName = null;
+        if (this.nameLabel != null || this.nameIcon == null || this.nameIcon.Length == 0) {
+            this.nameLabel.iconSprite.transform.localScale = new Vector3(0f, 0f, 0f);
+            this.nameLabel.iconName = null;
+            this.nameLabel.name = null;
         }
     }
     base.StartCoroutine(this.UpdateDistance());
