e91b463a8894eecef3669413da3503d99433294a
Latte <mertborainevi@gmail.com>
2025-10-02 01:43:55 +0200

    Increase scroll sensitivity in the inventory and crafting windows

---
+++ Base/CraftingWindow.Awake().cs
@@ -0,0 +1,8 @@
+private void Awake() {
+    this.primaryWindow = base.GetComponent<PrimaryWindow>();
+    this.primaryWindow.windowName = "crafting";
+    this.primaryWindow.maximizeIfTiny = true;
+    RectTransform rectTransform = this.primaryWindow.AddPanel("Panels/Parchment");
+    rectTransform.Find("Scroll Rect").GetComponent<ScrollRect>().scrollSensitivity = 15f;
+    ((RectTransform)rectTransform.Find("Scroll Rect/Content").transform).gameObject.AddComponent<CraftingItemContainer>().primaryWindow = this.primaryWindow;
+}
---
+++ Base/InventoryWindow.Awake().cs
@@ -0,0 +1,8 @@
+private void Awake() {
+    this.primaryWindow = base.GetComponent<PrimaryWindow>();
+    this.primaryWindow.windowName = "inventory";
+    this.primaryWindow.maximizeIfTiny = true;
+    RectTransform rectTransform = this.primaryWindow.AddPanel("Panels/Parchment");
+    rectTransform.Find("Scroll Rect").GetComponent<ScrollRect>().scrollSensitivity = 15f;
+    ((RectTransform)rectTransform.Find("Scroll Rect/Content").transform).gameObject.AddComponent<InventoryItemContainer>().primaryWindow = this.primaryWindow;
+}
