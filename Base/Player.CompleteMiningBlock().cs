9b23ff1345191a786b90372b242ebdeba868d90a
Latte <mertborainevi@gmail.com>
2025-10-02 17:15:51 +0200

    Implement pile use type for number of item drops

---
+++ Base/Player.CompleteMiningBlock().cs
@@ -0,0 +1,37 @@
+public bool CompleteMiningBlock(ZoneBlock block) {
+    Item.Layer layer = block.MineableLayer();
+    Item item = block.ItemByLayer(layer).RootItem();
+    Item primaryItem = this.primaryItem;
+    int layerMod = block.ModByLayer(layer);
+    int serverItemCode = (item.parentItem == null) ? item.code : item.parentItem.code;
+    Command.Send(Command.Identity.BlockMine, new object[] {
+        block.position.x,
+        block.position.y,
+        (int)layer,
+        serverItemCode,
+        0
+    });
+    bool digging = primaryItem.action == Item.Action.Dig && item.diggable;
+    int layerReplacementItemCode = (!digging) ? 0 : Item.GetCode("ground/earth-dug");
+    block.SetLayer(layer, layerReplacementItemCode, 0);
+    if (!digging) {
+        Item inventoryItem = (item.mod != Item.Mod.Decay || layerMod <= 0 || item.decayInventoryItem == null) ? item.inventoryItem : item.decayInventoryItem;
+        if (inventoryItem != null) {
+            int qty = 1;
+            int pile = item.use.GetInt("pile");
+            if (item.mod == Item.Mod.Stack && layerMod > 0) {
+                qty = layerMod;
+            }
+            if (pile != 0) {
+                qty = pile * layerMod;
+            }
+            this.inventory.Add(inventoryItem.code, qty);
+        }
+    }
+    int itemsMined = this.itemsMined;
+    this.itemsMined = itemsMined + 1;
+    block.Place(true);
+    Messenger.Broadcast<Item>("playerMinedItem", item);
+    Messenger.Broadcast<ZoneBlock>("playerMinedBlock", block);
+    return true;
+}
